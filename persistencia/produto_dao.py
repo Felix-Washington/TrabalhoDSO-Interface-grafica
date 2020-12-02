3229a14272034be8220fe16e8f3661cc66ac5ba1
Francielle Lopes <68799185+franciellelopes@users.noreply.github.com>
2020-12-02 13:42:57 -0300

    -Criação da classe produto_dao

---
+++ persistencia/produto_dao.py
@@ -0,0 +1,19 @@
+from persistencia.abstract_dao import AbstractDAO
+from entidade.produto import Produto
+
+class ProdutoDAO(AbstractDAO):
+    def __init__(self):
+        super().__init__("produtos.pkl")
+
+    def add(self, produto: Produto):
+        if (produto is not None) and (isinstance(produto, Produto)) and (
+        isinstance((produto.codigo, int))):
+            super().add(produto.codigo, produto)
+
+    def get(self, key: int):
+        if isinstance(key, int):
+            return super().get(key)
+
+    def remove(self, key):
+        if isinstance(key, int):
+            super().remove(key)
